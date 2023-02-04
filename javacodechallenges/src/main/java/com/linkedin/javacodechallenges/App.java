2a8d0efc9c841152cb78191debff06915f9b0997
Mauricio Molina <36119589+edymol@users.noreply.github.com>
2023-02-04 15:00:28 +0000

    complex password

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -8,15 +8,40 @@ import java.util.Scanner;
  */
 public class App
 {
-    public static boolean isEven(int n){
-        return n%2 == 0;
+    // public static boolean isEven(int n){
+    //     return n%2 == 0;
+    // }
+    public static boolean isPasswordComplex(String password){
+        if(password.length()< 6){
+            return false;
+        }
+        boolean hasLowercaseLetter = false;
+        boolean hasUppercaseLetter = false;
+        Boolean hasNunber = false;
+
+        for(int i = 0; i < password.length() || !hasLowercaseLetter || !hasUppercaseLetter
+        || !hasNunber; i++){
+            char current = password.charAt(i);
+            if(Character.isDigit(current)){
+                hasNunber = true;
+            }else if(Character.isUpperCase(current)){
+                hasUppercaseLetter = true;
+            }else if(Character.isLowerCase(current)){
+                hasLowercaseLetter =  true;
+            }
+        }
+        return hasLowercaseLetter && hasUppercaseLetter && hasNunber;
     }
+
     public static void main( String[] args )
     {   Scanner scanner = new Scanner(System.in);
 
-        System.out.println( "Enter a number: " );
-        int userN = scanner.nextInt();
-        System.out.println("Is the number even2?: " + isEven(userN));
+        // System.out.println( "Enter a number: " );
+        // int userN = scanner.nextInt();
+        // System.out.println("Is the number even2?: " + isEven(userN));
+        System.out.println("Enter a password: ");
+        String userPassword = scanner.nextLine();
+        System.out.println("Your password is strong? " + isPasswordComplex(userPassword));
 
         scanner.close();
     }
