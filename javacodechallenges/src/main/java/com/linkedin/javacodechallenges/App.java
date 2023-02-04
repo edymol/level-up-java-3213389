cb5e2c923bb19b501b4dbae417ee3d5c9150956d
Mauricio Molina <36119589+edymol@users.noreply.github.com>
2023-02-04 14:29:54 +0000

    odd or even

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,27 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public static boolean isEven(int n){
+        if(n%2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   Scanner scanner = new Scanner(System.in);
+
+        System.out.println( "Enter a number: " );
+        int userN = scanner.nextInt();
+        System.out.println("Is the number even2?: " + isEven(userN));
+
+        scanner.close();
     }
 }
